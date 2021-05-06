59982268f0bffb7b1c5cb8efc27c27d4e3d3c4e5
maotterson <otterson.2@gmail.com>
2021-05-05 22:38:57 -0400

    add bcrypt to project

--- srv/routes/api.js
+++ srv/routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const User = require("../models/User");
 const Reading = require("../models/Reading");
@@ -66,7 +69,7 @@ router.post('/users', (req, res, next ) => {
 //  POST /api/login
 //
 router.post('/login', (req, res, next ) => {
-  console.log(req)
+  console.log(req.body)
   res.status(200).json({
     message: "POST @ /login (login attempt)",
     body: req
